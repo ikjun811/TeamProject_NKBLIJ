90003821eacb8a554a3efceab7e42d72f7432dea
nkbnkb0920 <nkbnkb0920@naver.com>
2022-05-09 12:00:53 +0900

    Inventory 구현

--- Assets/UI/Touch_Panel.cs
+++ Assets/UI/Touch_Panel.cs
@@ -1,17 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class Touch_Panel : MonoBehaviour
 {
-    GameObject clikedObj;
+    UIManager um;
+
+    private GameObject clikedObj;
+    public Inventory inventory;
     private void Start()
     {
-        GameObject clikedObj = null;
+        clikedObj = null;
+        um = GameObject.Find("UIManager").GetComponent<UIManager>();
     }
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+
+        if (Input.GetMouseButtonDown(0) && !um.IsUIOn)
         {
             Vector2 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             RaycastHit2D hit = Physics2D.Raycast(pos, Vector2.zero);
@@ -20,9 +26,31 @@ public class Touch_Panel : MonoBehaviour
             {
                 clikedObj = hit.transform.gameObject;
                 Debug.Log(clikedObj.name);
+                if (clikedObj.name == "Lighter")
+                {
+                    inventory.AddItem(clikedObj.GetComponent<Item_PickUp>().item);
+                    Destroy(clikedObj);
+                }
+                else if (clikedObj.name == "Gas")
+                {
+                    inventory.AddItem(clikedObj.GetComponent<Item_PickUp>().item);
+                    Destroy(clikedObj);
+                }
+                else if (clikedObj.name == "DoorLock")
+                {
+                    // UI 팝업 출력 + 입력 받기 + 터치 제어
+                }
+                else if (clikedObj.name == "Book")
+                {
+                    // 대사 출력
+                }
+                else if (clikedObj.name == "Window")
+                {
+                    // 대사 출력
+                }
             }
+
         }
-        else clikedObj = null;
     }
 
 }
