417af38e8e6d0b27f0ca61858f3e5adde1a2acf9
ikjun811 <ikjun811@naver.com>
2022-05-11 03:44:06 +0900

    SlowEffect

--- Assets/Scenes/FirstStart_log/DialogueManager.cs
+++ Assets/Scenes/FirstStart_log/DialogueManager.cs
@@ -16,6 +16,9 @@ public class DialogueManager : MonoBehaviour
     bool isDialogue = false;
     bool isNext = false;
 
+    [Header("텍스트 딜레이")]
+    [SerializeField] float textDelay;
+
     int lineCount = 0;
     int contextCount = 0;
 
@@ -29,11 +32,35 @@ public class DialogueManager : MonoBehaviour
                 {
                     isNext = false;
                     txt_Dialogue.text = "";
-                    ++contextCount;
-                    StartCoroutine(TypeWriter());
+                    if (++contextCount < dialogues[lineCount].contexts.Length)
+                    {
+                        StartCoroutine(TypeWriter());
+                    }
+                    else
+                    {
+                        contextCount = 0;
+                        if(++lineCount< dialogues.Length)
+                        {
+                            StartCoroutine(TypeWriter());
+                        }
+                        else
+                        {
+                            EndDialogue();
+                        }
+                    }
                 }
             }
         }
+    }
+
+    void EndDialogue()
+    {
+        isDialogue = false;
+        contextCount = 0;
+        lineCount = 0;
+        dialogues = null;
+        isNext = false;
+        SettingUI(false);
     }
 
     public void ShowDialogue(Dialogue[] p_dialogues)
@@ -56,15 +83,21 @@ public class DialogueManager : MonoBehaviour
         string t_ReplaceText = dialogues[lineCount].contexts[contextCount];
         t_ReplaceText = t_ReplaceText.Replace("'", ",");
 
-        txt_Dialogue.text = t_ReplaceText;
+
+        txt_Name.text = dialogues[lineCount].name;
+        for(int i = 0; i< t_ReplaceText.Length; i++)
+        {
+            txt_Dialogue.text += t_ReplaceText[i];
+            yield return new WaitForSeconds(textDelay);
+        }
 
         isNext = true;
-        yield return null;
+
     }
 
     void SettingUI(bool p_flag)
     {
-        go_DialogueBar.SetActive(p_flag);
+        go_DialogueBar.SetActive(p_flag); 
         go_DialogueNameBar.SetActive(p_flag);
     }
 }
